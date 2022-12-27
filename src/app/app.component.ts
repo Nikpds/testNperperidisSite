065385e9a9f31b1cb4f5234b2e1a51a1c1c92244
Nikolaos Perperidis <Nikolaos.Perperidis@fenergo.com>
2022-12-27 09:22:21 +0200

    sync

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { SwPush, SwUpdate } from '@angular/service-worker';
-import { NewsletterService } from './newsletter.service';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging, getToken } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDN8rXxoRW6Jdtf14nZpA2tew4SfjszSOc",
+  authDomain: "push-notification-6670c.firebaseapp.com",
+  projectId: "push-notification-6670c",
+  storageBucket: "push-notification-6670c.appspot.com",
+  messagingSenderId: "729304950082",
+  appId: "1:729304950082:web:c09f6dc65c92e9eae614db",
+  measurementId: "G-L2QZ0Z06GZ"
+};
 
 @Component({
   selector: 'app-root',
@@ -11,40 +22,40 @@ import { NewsletterService } from './newsletter.service';
 export class AppComponent {
   readonly server = 'https://www.cross-arm.gr/api/';
   title = 'angular-pwa-app';
-  readonly VAPID_PUBLIC_KEY =
-    'BFjpqEBTdSG3dJU06X6b2U6VuBXx6RE2lcCWLZXJbMU2lph-6elhLOQhNGpirmo9PkeCVehJ6Cc7_idsJtTudeU';
   email?: string;
+
 
   constructor(
     private http: HttpClient,
-    private swPush: SwPush,
-    private newsletterService: NewsletterService
-  ) {}
+  ) {
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
 
-  ngOnInit() {}
+    const messaging = getMessaging();
+    // Add the public key generated from the console here.
+    getToken(messaging, { vapidKey: "BJZJnCBFZEotOw_8mxfkkSJ06Wj8RiyhF_ZuW5g4h94DGOGGrG7jbqbpaEcwyjuw05cj91ImQn-vSjBkON4iPlc" }).then(res => {
+      console.log(res)
+    }).catch(err => console.log(err));
+
+  }
+
+  ngOnInit() {
+
+  }
 
   checkEmail() {
     return this.http.post(this.server + 'athlete/validate/subscription', {
       key: 'MFswDQYJKoZIhvcNAQEBBQADSgAwRwJAcrcPVP4wNgtzHCsVYb2xWsEd7jJVYd+p',
       email: this.email
-    });
+    }).subscribe(res => console.log(res));
   }
 
   async subscribeToNotifications() {
-    try {
-      await this.swPush
-        .requestSubscription({
-          serverPublicKey: this.VAPID_PUBLIC_KEY,
-        })
-        .then((sub) => {
-          console.log(sub);
-          this.newsletterService.addPushSubscriber(sub).subscribe();
-        })
-        .catch((err) =>
-          console.log('Could not subscribe to notifications', err)
-        );
-    } catch (err) {
-      console.error('Could not subscribe due to:', err);
-    }
+    console.log('Requesting permission...');
+    Notification.requestPermission().then((permission) => {
+      if (permission === 'granted') {
+        console.log('Notification permission granted.');
+      }
+    });
   }
 }
